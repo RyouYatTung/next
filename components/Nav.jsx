7ef00cfd16e09ac361cac278f010fd2bf625986b
RyouYatTung <leungyattung1108@gmail.com>
2023-07-07 00:00:54 +0900

    first commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,12 +7,91 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
   const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+  const [toggleDropDown, setToggleDropdown] = useState(false);
+
+  // useEffect(() => {
+  //   const setProviders = async () => {
+  //     const response = await getProviders();
+
+  //     setProviders(response);
+  //   };
+
+  //   setProviders();
+  // }, []);
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image src="/assets/images/logo.svg" alt="logo" width="40" height="40" className="object-contain" />
         <p>Next.js</p>
       </Link>
+
+      {/* Mobile Nav */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Creat Post
+            </Link>
+
+            <button type="button" className="outline_btn" onClick={signOut}>
+              Sign Out
+            </button>
+
+            <Link href="/preofile">
+              <Image src="/assets/images/logo.svg" width={30} height={30} className="rounded-full" alt="profile" />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button type="button" key={provider.name} className="black_btn" onClick={() => signIn(provider.id)}>
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image src="/assets/images/logo.svg" width={30} height={30} className="rounded-full" alt="profile" onClick={() => setToggleDropdown((prew) => !prew)} />
+
+            {toggleDropDown && (
+              <div className="dropdown">
+                <Link href="/profile" className="dropdown_link" onClick={() => setToggleDropdown(false)}>
+                  My Profile
+                </Link>
+                <Link href="/profile-prompt" className="dropdown_link" onClick={() => setToggleDropdown(false)}>
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button type="button" key={provider.name} className="black_btn" onClick={() => signIn(provider.id)}>
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
--- components/promptCard.jsx
+++ components/promptCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const promptCard = () => {
+const PromptCard = () => {
   return <div>promptCard</div>;
 };
 
-export default promptCard;
+export default PromptCard;
